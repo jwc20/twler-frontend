e199a7c80d07465f1642f90a96cda582db29a4cf
jwc20 <choij2015@gmail.com>
2022-05-17 14:07:38 -0700

    working on d3.js scatterplot graph for results

--- src/components/ExampleGraph.js
+++ src/components/ExampleGraph.js
@@ -1,34 +1,24 @@
-
 import React, { useState, useRef, useEffect } from "react";
 import * as d3 from "d3";
-
-
-
+import sample from "../db/sample.json";
 
 function ExampleGraph() {
-  const [data] = useState([25, 50, 35, 15, 94, 10]);
+  const [data, setData] = useState(sample);
   const svgRef = useRef();
 
   useEffect(() => {
-    /* *
-     * 1. Setting up the svg
-     * 2. Setting the scaling
-     * 3. Setting the Axes
-     * 4. Setting up the data for the svg
-     * */
-
-    // setting up svg
-    const w = 400;
-    const h = 100;
+    console.log(data)
+    const w = 11400;
+    const h = 600;
     const svg = d3
       .select(svgRef.current)
       .attr("width", w)
       .attr("height", h)
       .style("background", "#d3d3d3")
       .style("margin-top", "50")
+      .style("margin-left", "50")
       .style("overflow", "visible");
 
-    // setting the scaling
     const xScale = d3
       .scaleLinear()
       .domain([0, data.length - 1])
@@ -40,7 +30,6 @@ function ExampleGraph() {
       .y(yScale)
       .curve(d3.curveCardinal);
 
-    // setting the axes
     const xAxis = d3
       .axisBottom(xScale)
       .ticks(data.length)
@@ -49,19 +38,16 @@ function ExampleGraph() {
     svg.append("g").call(xAxis).attr("transform", `translate(0, ${h})`);
     svg.append("g").call(yAxis);
 
-    // setting up the data for the svg
-    svg
-      .selectAll(".line")
-      .data([data])
-      .join("path")
-      .attr("d", (d) => generateScaledLine(d))
-      .attr("fill", "none")
-      .attr("stroke", "black");
+
+
+
+
+
   }, [data]);
 
   return (
     <div>
-      <svg className="graph" ref={svgRef}></svg>
+      <svg ref={svgRef}></svg>
     </div>
   );
 }
