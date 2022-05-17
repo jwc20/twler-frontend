a296a0aa7f6fbdfa1ef92641124b0beb433fdbd7
jwc20 <choij2015@gmail.com>
2022-05-17 11:19:13 -0700

    fixes with routes and navbar (using react-router-dom@v6

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Nav />}>
+          <Route index element={<Home />} />
+
+          {/*
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          */}
+          {/*
+        <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route>
+        */}
+        </Route>
+      </Routes>
       <header className="App-header">
         <h1 className="text-3xl font-bold underline italic">Hello world!</h1>
       </header>
---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
