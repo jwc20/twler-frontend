9ffce6fa059c51a6ae5392562478f588f80d6291
jwc20 <choij2015@gmail.com>
2022-05-26 15:16:08 -0700

    added search filter to EventTable

--- src/components/EventTable.js
+++ src/components/EventTable.js
@@ -1,33 +1,67 @@
 import { useState, useEffect } from "react";
-import { useTable } from "react-table";
+import { useTable, useGlobalFilter, useAsyncDebounce } from "react-table"; // new
 
 const url = "http://127.0.0.1:3000";
 
-function EventTable({ columns, events }) {
-  // Fetch years for dropdown menu
-  // Fetch Events from the latest year
-  // Render
-  //
+function GlobalFilter({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const count = preGlobalFilteredRows.length;
+  const [value, setValue] = useState(globalFilter);
+  const onChange = useAsyncDebounce((value) => {
+    setGlobalFilter(value || undefined);
+  }, 200);
 
+  return (
+    <span>
+      Search:{" "}
+      <input
+        value={value || ""}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        placeholder={`${count} records...`}
+      />
+    </span>
+  );
+}
+
+function EventTable({ columns, events }) {
+  /*
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({
       columns,
       data: events,
     });
-
-  /*
-  const [years, setYears] = useState([]);
-
-  useEffect(() => {
-    fetch(url + "/years")
-      .then((r) => r.json())
-      .then(setYears);
-  }, []);
-
   */
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state, // new
+    preGlobalFilteredRows, // new
+    setGlobalFilter, // new
+  } = useTable(
+    {
+      columns,
+      data: events,
+    },
+    useGlobalFilter // new
+  );
 
   return (
     <div>
+      <GlobalFilter
+        preGlobalFilteredRows={preGlobalFilteredRows}
+        globalFilter={state.globalFilter}
+        setGlobalFilter={setGlobalFilter}
+      />
       <table {...getTableProps()} border="1">
         <thead>
           {headerGroups.map((headerGroup) => (
